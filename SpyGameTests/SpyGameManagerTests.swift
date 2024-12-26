af8228b60e6c57ce3ac60cb544e8ffb812123ba8
Amin <32193878+bakhshaei@users.noreply.github.com>
2024-12-26 10:25:15 +0330

    Chagne tests to adopt more with SwifTesting.

--- SpyGameTests/SpyGameManagerTests.swift
+++ SpyGameTests/SpyGameManagerTests.swift
@@ -14,23 +14,20 @@ struct SpyGameManagerTests {
     @Test func createInvalidSpyGame() throws {
         
         var gameManager : SpyGameManager? = nil
-        do {
+        #expect(throws: SpyGameError.numberOfSpiesShouleBeLessThanNumberOfPeople) {
             gameManager = try SpyGameManager(numberOfParticipants: 4, numberOfSpies: 4)
         }
-        catch {
-            #expect(error as! SpyGameError == SpyGameError.numberOfSpiesShouleBeLessThanNumberOfPeople)
-        }
+        
         #expect(gameManager == nil)
+        
     }
     
     @Test func createInvalidSpyGameWithZeroSpies() throws {
         
         var gameManager : SpyGameManager? = nil
-        do {
+        
+        #expect(throws: SpyGameError.gameShouldHaveAtLeastOneSpy) {
             gameManager = try SpyGameManager(numberOfParticipants: 4, numberOfSpies: 0)
-        }
-        catch {
-            #expect(error as! SpyGameError == SpyGameError.gameShouldHaveAtLeastOneSpy)
         }
         #expect(gameManager == nil)
     }
@@ -45,17 +42,20 @@ struct SpyGameManagerTests {
     ])  func generateSpyGameTester(participant: Int, spy: Int) {
 
         var spies = spy
-        let gameManager = try! SpyGameManager(numberOfParticipants: participant, numberOfSpies: spies)
-        
-        for i in 0..<participant {
-            let participant = try? gameManager.getParticipant(at: i)
-            if participant == .spy {
-                spies -= 1
+        #expect(throws: Never.self) {
+            let gameManager = try SpyGameManager(numberOfParticipants: participant, numberOfSpies: spies)
+            
+            for i in 0..<participant {
+                let participant = try? gameManager.getParticipant(at: i)
+                if participant == .spy {
+                    spies -= 1
+                }
             }
         }
         
         #expect(spies == 0)
     }
+    
     
 
 }
